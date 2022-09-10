601c20eef3af62d9a46a132f948348093d06e282
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-10 14:52:09 -0300

    implementacao da API

--- demo/src/main/java/com/example/demo/TodoListApplication.java
+++ demo/src/main/java/com/example/demo/TodoListApplication.java
@@ -13,22 +13,6 @@ import java.time.LocalDateTime;
 @SpringBootApplication
 public class TodoListApplication {
 
-	@Autowired
-	private TodoRepository todoRepository;
-
-	@Bean
-	public CommandLineRunner init() {
-		return new CommandLineRunner() {
-			@Override
-			public void run(String... args) throws Exception {
-				Todo todo = new Todo();
-				todo.setDescription("Estudar Spring");
-				todo.setCreatedDate(LocalDateTime.now());
-				todoRepository.save(todo);
-			}
-		};
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
---
+++ demo/src/main/java/com/example/demo/rest/TodoController.java
@@ -0,0 +1,29 @@
+package com.example.demo.rest;
+
+import com.example.demo.model.Todo;
+import com.example.demo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository repository;
+
+    @PostMapping
+    public Todo save(@RequestBody Todo todo) {
+        return repository.save(todo);
+    }
+
+    @GetMapping("{id}")
+    public Todo getById(Long id) {
+        return repository
+                .findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+}
