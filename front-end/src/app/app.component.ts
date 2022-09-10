aaeb81da8398e7409f06ea7e7b887f6575ea4258
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-10 17:18:29 -0300

    criando a listagem dos items

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -9,17 +9,19 @@ import { TodoService } from './todo.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private todoService: TodoService) {}
+  todos: Todo[] = [];
 
   form: FormGroup = new FormGroup({
     description: new FormControl(''),
   });
+  constructor(private todoService: TodoService) {}
 
   submit() {
     console.log(this.form.value);
     const todo: Todo = { ...this.form.value };
-    this.todoService
-      .salvar(todo)
-      .subscribe((savedTodo) => console.log(savedTodo));
+    this.todoService.salvar(todo).subscribe((savedTodo) => {
+      this.todos.push(savedTodo);
+      this.form.reset();
+    });
   }
 }
