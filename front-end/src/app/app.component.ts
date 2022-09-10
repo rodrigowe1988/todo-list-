5092dec909d1b7eb5d1f498fe1ddecb665e552f0
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-09-10 16:07:00 -0300

    trabalhando com formularios

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  mensagem: string = 'Hello world';
+
+  constructor(){}
+  
+  form: FormGroup = new FormGroup({
+    description: new FormControl(''),
+  });
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
